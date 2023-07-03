e99da63e682ac7982ec10e1edc52080130a62244
Mistsoul <ugurcancamm@gmail.com>
2023-07-03 23:26:11 +0300

    Frontend Giriş
    Layout ve shared ayarlandı

---
+++ Frontend/OtelRezervasyon.webui/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OtelRezervasyon.webui.Controllers
+{
+    
+    public class AdminController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
